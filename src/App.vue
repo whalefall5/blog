5ce9750d8c4e48835116e09b61b0650d74d6c1b0
whalefall <6568233+whalefallaa@user.noreply.gitee.com>
2020-04-29 11:54:56 +0800

    ll

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,43 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+      <el-menu
+  :default-active="activeIndex2"
+  class="el-menu-demo"
+  mode="horizontal"
+  @select="handleSelect"
+  background-color="#545c64"
+  text-color="#fff"
+  active-text-color="#ffd04b">
+  <el-menu-item index="1">首页</el-menu-item>
+   <el-menu-item index="2" >添加博客</el-menu-item>
+  <el-menu-item index="3" >归档</el-menu-item>
+  <el-menu-item index="4"><a >关于山猪</a></el-menu-item>
+  <el-menu-item  ><el-input v-model="input" placeholder="请输入内容"></el-input></el-menu-item>
+</el-menu>
+  <bg></bg>
   </div>
 </template>
+<script>
+import bg from "./view/background"
+export default {
+  data () {
+    return {
+    
+    }
+  },
+  components: {
+    bg
+  }
+  
+}
+</script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*{
+  margin: 0;
+  padding: 0;
 }
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/view/background.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="app">
+      <div  class="bg"  v-if="!background"></div>
+      <div class="lampon" @click="changebg"></div>
+<div id="e_background"  >
+	<div id="e_smallstars"></div>
+	<div id="e_moon"></div>
+</div>
+  </div>
+</template>
+
+<script>
+import './../assets/css/main.css'
+import './../assets/css/style.css'
+
+export default {
+  name: 'Bg',
+  data () {
+    return {
+        background:false
+    };
+  },
+  components: {},
+  mounted () {
+     
+  },
+  methods: {
+      
+       changebg(){
+          this.background=!this.background
+          console.log(this.background)
+      }
+  }
+}
+</script>
+
+<style  scoped>
+
+.bg{
+  background-image: url('./../assets/img/index.png');
+   height: 1000px;
+}
+.lampon  {
+  width: 80px;
+  height: 160px;
+  background: url('./../assets/img/lampoff.4ca4d48a.png');
+  background-size: 100% 100%;
+  position: ;position: absolute;
+  top: 60px;
+  left: 20px;
+  
+}
+.lampon:hover{
+  background: url('./../assets/img/lampon.372e8604.png');
+   background-size: 100% 100%;
+   width: 80px;
+  height: 200px;
+}
+.e_background{
+    position: relative;
+    z-index: 10;
+}
+</style>
